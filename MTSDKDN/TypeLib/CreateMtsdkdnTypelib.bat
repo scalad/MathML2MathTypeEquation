9b26ffb6ed087eaa3306a3ff28deddd16597c7ab
techjintz <jintz_1215@163.com>
2017-08-03 11:10:53 +0800

    MTSDK comes with MathType Sdk

---
+++ MTSDKDN/TypeLib/CreateMtsdkdnTypelib.bat
@@ -0,0 +1,7 @@
+@rem CreateMtsdkdnTypelib.bat --- Copyright (c) 2008-2010 by Design Science, Inc.
+@rem Purpose:
+@rem $Header: /MathType/Windows/SDK/DotNET/MTSDKDN/TypeLib/CreateMtsdkdnTypelib.bat 4     3/30/10 8:43a Jimm $
+
+"C:\Program Files\Microsoft SDK\Bin\mktyplib.exe" MTSDKDN.odl /nocpp /tlb MTSDKDN.tlb
+
+@pause
